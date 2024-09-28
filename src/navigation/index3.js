344e13bd3a707e3128293dce566ba0b53d2d2cde
Aniket Kumar <kraniket123654@gmail.com>
2024-09-28 23:26:17 +0530

    added tailwaindcss confi

--- src/navigation/index3.js
+++ src/navigation/index3.js
@@ -1,52 +1,43 @@
 // This is for login
-
+import React from 'react';
 import {
-    View,
-    Text,
-    SafeAreaView,
-    StyleSheet,
-    TouchableOpacity,
-    BackHandler,
-  } from "react-native";
-  
-  // navigation import
-  import { createNativeStackNavigator } from '@react-navigation/native-stack';
-  import { NavigationContainer } from "@react-navigation/native";
-  
-  const Stack = createNativeStackNavigator();
-  
-  // Bottom Tabs
+  View,
+  Text,
+} from 'react-native';
+
+// navigation import
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
+
+// Bottom Tabs
 //   import BottomTabs from './BottomTabs'
-  import Login from '../screens/auth/LogIn'
-  import SignUp from '../screens/auth/SignUp'
-  
-  
-  
-  function Home() {
-    return (
-      <View>
-        <Text>index</Text>
-      </View>
-    )
-  }
-  
-  export default function AppNavigation3() {
-    return (
-  
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            contentStyle: {
-              backgroundColor: "red",
-            },
-            headerShown: false,
-          }}
-          initialRouteName={Login}
-        >
-          <Stack.Screen name="LogIn" component={Login} />
-          <Stack.Screen name="SignUp" component={SignUp} />
-        </Stack.Navigator>
-      </NavigationContainer>
-  
-    )
-  }
+import Login from '../screens/auth/LogIn';
+import SignUp from '../screens/auth/SignUp';
+
+
+
+
+export default function AppNavigation3() {
+  return (
+
+    <NavigationContainer>
+      <Stack.Navigator
+
+        screenOptions={{
+          contentStyle: {
+            backgroundColor: '#FFF',
+          },
+          headerShown: false,
+        }}
+        initialRouteName={Login}
+
+      >
+        <Stack.Screen name="LogIn" component={Login} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  );
+}
--- src/screens/auth/LogIn.js
+++ src/screens/auth/LogIn.js
@@ -1,14 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StatusBar, Text, View } from 'react-native';
+import React from 'react';
 
 const LogIn = () => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View className="bg-black">
+      <Text >Logjkhkkjkin</Text>
+      <View>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
-  )
-}
+    </View>
+  );
+};
 
-export default LogIn
-
-const styles = StyleSheet.create({})
+export default LogIn;
