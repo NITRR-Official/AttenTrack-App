7aa8bcb050ad9d06e0b7524f1603b361b88e1e9c
harshdew02 <harshdewangan032@gmail.com>
2024-09-29 18:45:28 +0530

    create student page added

--- src/screens/teacher/CreateClass.js
+++ src/screens/teacher/CreateClass.js
@@ -12,13 +12,16 @@ import {
     BackHandler,
     ScrollView,
 } from 'react-native';
-import { XMarkIcon } from 'react-native-heroicons/outline';
+import { ArrowUpTrayIcon, XMarkIcon } from 'react-native-heroicons/outline';
 import {
     widthPercentageToDP as wp,
     heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 import { theme } from '../../theme';
 const CreateClass = () => {
+
+    const [number, onChangeNumber] = React.useState('');
+
     return (
         <SafeAreaView style={{ flex: 1, alignItems: 'center' }} >
             <View style={{ backgroundColor: '#fff', width: wp(100), height: hp(6), display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: wp(8) }} >
@@ -32,8 +35,18 @@ const CreateClass = () => {
 
             <TextInput
                 placeholder="Class Name"
-                style={{ borderWidth: 1, borderColor: theme.maincolor, width: wp(90), height: hp(7), borderRadius: wp(2), paddingHorizontal: wp(4), marginTop: wp(8), color: theme.maincolor }}
+                placeholderTextColor='#c7c7c7'
+                onChangeText={onChangeNumber}
+                value={number}
+                keyboardType="text"
+                style={{ borderWidth: 1, borderColor: theme.maincolor, width: wp(90), height: hp(7), borderRadius: wp(2), paddingHorizontal: wp(4), marginTop: wp(8), color: theme.maincolor, fontSize: wp(5), fontWeight: '500' }}
             />
+
+            <TouchableOpacity style={{ height: hp(7), width: wp(44), backgroundColor: theme.maincolor, borderRadius: wp(2), justifyContent: 'space-between', alignItems: 'center', marginTop: wp(8), display: 'flex', flexDirection: 'row', paddingHorizontal: wp(4) }} >
+                <Text style={{ color: "#fff", fontSize: wp(4), fontWeight: '200', width: wp(22), textAlign: 'center' }} >Upload Excel Sheet</Text>
+                <ArrowUpTrayIcon size={wp(8)} color="#fff" />
+            </TouchableOpacity>
+
 
             <ScrollView
                 scrollEventThrottle={1}
@@ -50,4 +63,12 @@ const CreateClass = () => {
 
 export default CreateClass
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        color: theme.maincolor
+    },
+});
